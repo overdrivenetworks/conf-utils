1de6acbde8c0290f394aac04b1b3fddd5322a236
James Lu <james@overdrivenetworks.com>
2017-08-08 10:52:20 -0700

    Refresh sync tools
    - Fix shellcheck warnings
    - Configurable temp dir
    - More concise logging of the source and target paths

--- scripts/sync-motd.sh
+++ scripts/sync-motd.sh
@@ -4,12 +4,16 @@
 . scripts/config.sh
 
 motdsync () {
-	echo "Sync MOTD: $1"
-	scp ${options[$1]} $1.motd ${servers[$1]}:${targetpath}/ircd.motd
-	scp ${options[$1]} ircd.rules ${servers[$1]}:${targetpath}/ircd.rules
+	echo "Sync MOTD: $1.motd ircd.rules"
+
+        # shellcheck disable=SC2086
+        # options are purposely designed be word-split
+	scp ${options[$1]} "$1.motd" "${servers[$1]}:${targetpath}/ircd.motd"
+        # shellcheck disable=SC2086
+	scp ${options[$1]} ircd.rules "${servers[$1]}:${targetpath}/ircd.rules"
 }
 
-if [[ -z $1 ]]; then
+if [[ -z "$1" ]]; then
 	for server in "${!servers[@]}"
 	do
 		motdsync "$server"
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ `git symbolic-ref --short -q HEAD` != master ]] && [[ $1 != "force" ]]; then
+if [[ "$(git symbolic-ref --short -q HEAD)" != master ]] && [[ $1 != "force" ]]; then
 	echo "Aborting sync since we're not on branch 'master'. Run --force to sync anyways."
 	exit 1
 fi
@@ -7,19 +7,40 @@ fi
 # Grab our config
 . scripts/config.sh
 
-mkdir -p tmp/
+if [[ -z "$tmpfolder" ]]; then
+	tmpfolder="tmp"
+fi
+
+if [[ -z "$targetpath" ]]; then
+	echo "No target path specified in conf?"
+	exit 1
+fi
+
+mkdir -p "$tmpfolder"
 
 csync () {
-	echo "Sync: $1"
-	getconfig $1 > tmp/$1.conf
-	scp ${options[$1]} tmp/$1.conf ${servers[$1]}:${targetpath}/inspircd.conf
-	rm tmp/$1.conf
+	# XXX: make this configurable, but in a backwards-compatible fashion
+	targetfile="inspircd.conf"
+
+	echo "Sync: $targetfile @ $1"
+	_TMPFILE="$tmpfolder/$1.conf"
+	_REAL_TARGETFILE="${servers[$1]}:${targetpath}/${targetfile}"
+
+	# Write the config file to a temporary file.
+	getconfig "$1" > "$_TMPFILE"
+
+	echo "Transfering $_TMPFILE => $_REAL_TARGETFILE"
+	# shellcheck disable=SC2086
+	# options are purposely designed be word-split
+	scp ${options[$1]} "$_TMPFILE" "$_REAL_TARGETFILE"
+	rm "$_TMPFILE"
 }
 
 if [[ -z $1 ]]; then
 	for server in "${!servers[@]}"
 	do
 		csync "$server"
+		echo ""
 	done
 else
 	csync "$1"
